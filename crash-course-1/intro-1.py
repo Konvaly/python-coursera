16f38e253af287775a38d8534dba8a8668c7cd04
Konvaly <konvaly@gmail.com>
2025-08-25 15:04:28 +0100

    data types-3

--- crash-course-1/intro-1.py
+++ crash-course-1/intro-1.py
@@ -34,3 +34,26 @@ share_per_person = total/room_guests
 # This line outputs the result of the final calculation stored
 # in the variable "share_per_person"
 print("Each person needs to pay: " + str(share_per_person)) # change a data type
+
+
+
+#Output multiple string variables on a single line to form a sentence
+
+#Use the plus (+) connector or a comma to connect strings in a print() function
+
+#Create spaces between variables in  a print() function
+
+# The following 5 lines assign strings to a list of variables.
+salutation = "Dr."
+first_name = "Prisha"
+middle_name = "Jai"
+last_name = "Agarwal"
+suffix = "Ph.D."
+ 
+print(salutation + " " + first_name + " " + middle_name + " " + last_name + ", " + suffix) 
+# The comma as a string ", " adds the conventional use of a comma plus a 
+# space to separate the last name from the suffix.
+ 
+# Alternatively, you could use commas in place of the + connector:
+print(salutation, first_name, middle_name, last_name,",", suffix)
+# However, you will find that this produces a space before a comma within a string.
