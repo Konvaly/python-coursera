f53c47e4ed0fcb07a8f9db20d9fa2950e440dda4
Konvaly <konvaly@gmail.com>
2025-08-25 15:02:45 +0100

    data types-2

--- crash-course-1/intro-1.py
+++ crash-course-1/intro-1.py
@@ -20,3 +20,17 @@ tuple_of_numbers: typing.Tuple[int, int, int] = (1, 2, 3)
 dictionary: typing.Dict[str, int] = {"key1": 1, "key2": 2}
 # Define a variable of type set
 set_of_numbers: typing.Set[int] = {1, 2, 3}
+
+# The following lines assign the variable to the left of the = 
+# assignment operator with the values and arithmetic expressions 
+# on the right side of the = assignment operator.
+hotel_room = 100
+tax = hotel_room * 0.08
+total = hotel_room + tax
+room_guests = 4
+share_per_person = total/room_guests
+
+
+# This line outputs the result of the final calculation stored
+# in the variable "share_per_person"
+print("Each person needs to pay: " + str(share_per_person)) # change a data type
