424bc365dae854274ed521ccd03754c05ef97074
Konvaly <konvaly@gmail.com>
2025-08-27 13:40:44 +0100

    Rewrited task-3

--- crash-course-1/intro-1.py
+++ crash-course-1/intro-1.py
@@ -97,3 +97,11 @@ def hint_username(username):
 
 hint_username("Lena")
 
+# REWRITE THE CODE ABOVE 
+def hint_username(username):
+    if len(username) < 3:
+        print("Invalid username. Must be at least 3 characters long")
+    elif len(username) > 15:
+        print("Invalid username. Must be at most 15 characters long")
+    else:
+        print("Valid username")
