3f5d32fb7d6182e134497574cdcc56776b9c9134
Konvaly <konvaly@gmail.com>
2025-08-30 00:46:50 +0100

    Completed task 3

--- loops/while-loop.py
+++ loops/while-loop.py
@@ -22,3 +22,20 @@ my_variable = 5
 while my_variable < 10:
     print("Hello")
     my_variable += 1
+
+
+##
+# task-3
+x = 1
+sum = 0
+while x < 10:
+    sum = sum + x
+    x = x + 1
+
+product = 1
+while x < 10:
+    product = product * x
+    x = x + 1
+
+print(sum, product)
+#Ouput 45 1
