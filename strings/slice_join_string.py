5bc385b1264111ac01cc822b377055c0e277a0b5
Konvaly <konvaly@gmail.com>
2025-08-31 14:31:03 +0100

    Added file slice join string with an example-2 and 3

--- strings/slice_join_string.py
+++ strings/slice_join_string.py
@@ -5,3 +5,12 @@ print(string1[0])   # Prints “G”
 print(string1[4:8]) # Prints “ting”
 print(string1[11:]) # Prints “Earthlings”
 print(string1[:5])  # Prints “Greet”
+
+# Example 2
+# Prints “Earthlings” again
+print(string1[-10:])
+
+# Example 3
+# If your index is beyond the end of the string, Python returns an empty string.
+# Prints “” 
+print(string1[55:])
