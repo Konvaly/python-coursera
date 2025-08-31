19825dea8f0563489f8a592508d14c0ac0b5d9fb
Konvaly <konvaly@gmail.com>
2025-08-31 14:35:42 +0100

    Added file slice join string with an example-4

--- strings/slice_join_string.py
+++ strings/slice_join_string.py
@@ -14,3 +14,14 @@ print(string1[-10:])
 # If your index is beyond the end of the string, Python returns an empty string.
 # Prints “” 
 print(string1[55:])
+
+# Example 4
+# An optional way to slice an index is by the stride argument, 
+# indicated by using a double colon. 
+# This allows you to skip over the corresponding number of characters in your index, 
+# or if you’re using a negative stride, the string prints backwards.
+# Prints “Getns atlns”
+print(string1[0::2])
+
+# Prints “sgnilhtraE ,sgniteerG”
+print(string1[::-1])
