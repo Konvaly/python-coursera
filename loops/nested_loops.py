dbb3452f60d636844824f41b5462b5d5b2c52b5f
Konvaly <konvaly@gmail.com>
2025-08-31 17:07:48 +0100

    Added file nested loops with an example-3

--- loops/nested_loops.py
+++ loops/nested_loops.py
@@ -12,3 +12,18 @@ for home_team in teams:
     if home_team != away_team:
       print(home_team + " vs " + away_team)
 
+# Example-3
+# Example of nested for loops:
+# This code demonstrates the outer and inner loop iterations of a pair 
+# of nested for loops. Click "Run" to see the results. The outer loop
+# will run twice for the range pointer positions [0, 1] in range(2).
+# The inner loop will run 4 times for the range pointer positions 
+# [0, 1, 2, 3] in range(3+1) or range(4) each time the outer loop runs.
+# So, the inner loop will execute 8 times in total.
+
+for x in range(2):
+  print("This is the outer loop iteration number " + str(x))
+  for y in range(3+1):
+    print("Inner loop iteration number " + str(y))
+print("Exit inner loop")
+
