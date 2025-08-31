68f68d1609172f61e9da4d00d4b82a260e5a2b12
Konvaly <konvaly@gmail.com>
2025-08-31 15:01:45 +0100

    Added file slice join string with an example-6

--- strings/slice_join_string.py
+++ strings/slice_join_string.py
@@ -35,3 +35,39 @@ print(" ".join(greetings))  # Prints "Hello world"
 # You can also concatenate a combination of strings and variables like in the following example.
 name = "Alice"
 print("Hello, " + name + "!")  # Prints "Hello, Alice!"
+
+# Example 6
+# How to combine slicing and joining strings
+# Now you know how to slice strings and join strings. 
+# Now, let’s put the two operations together by taking an unformatted phone number, 2025551212, 
+# and return it as a properly formatted U.S. number. In this example, 
+# we’ll use phonenum to refer to the unformatted phone number. 
+# You can define a function format_phone() to perform the whole operation. Here is a breakdown of the steps.
+
+# This slices the first three numbers from the string:
+phonenum = '2025551212'
+
+# The first 3 digits are the area code:
+area_code = "(" + phonenum[:3] + ")"
+
+# This slices the numbers 4–6 from the string.
+# The next 3 digits are called the “exchange”:
+exchange = phonenum[3:6]
+
+# Negative indexing counts backwards from the end of the numbers, 
+# slicing the last four numbers in the string.
+# The last 4 digits are the line number:
+line = phonenum[-4:]
+
+# And you can put them all together:
+# Put the pieces back together into a nicely formatted string:
+area_code + " " + exchange + "-" + line
+
+# When all the steps are assembled into a function:
+def format_phone(phonenum):
+    area_code = "(" + phonenum[:3] + ")"
+    exchange = phonenum[3:6]
+    line = phonenum[-4:]
+    return area_code + " " + exchange + "-" + line
+
+format_phone('2025551212')
