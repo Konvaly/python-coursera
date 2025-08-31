ea0e4a84eef3050a062eec180bf462178381cb09
Konvaly <konvaly@gmail.com>
2025-08-31 13:40:17 +0100

    Added file looping over string with an example-2

--- loops/looping_over_string.py
+++ loops/looping_over_string.py
@@ -1,3 +1,12 @@
+# Exanple 1
+
 greeting = 'Hello'
 for char in greeting:
 	print(char)
+	
+# Example 2
+#What if you don’t want the elements of the string but the position of the string instead? 
+# You’re in luck because Python can work that magic too!
+
+for i in range(len(greeting)):
+	print(i)
