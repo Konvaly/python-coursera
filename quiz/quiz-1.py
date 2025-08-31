ba53047d322e3e4ab5d50afe7438bbe3c691c9b2
Konvaly <konvaly@gmail.com>
2025-09-01 00:17:09 +0100

    Added file quiz with an example-4

--- quiz/quiz-1.py
+++ quiz/quiz-1.py
@@ -69,3 +69,48 @@ matrix(1, 4)
 # 2 4 6 8 
 # 3 6 9 12 
 # 4 8 12 16 
+
+#
+#Example-3
+# For this example, the outer for loop uses an end of range index of 
+# 10. The value of index 10 will be 10-1, or 9.  
+for outer_loop in range(10):
+
+    # Using the "outer_loop" variable as the end of range for the  
+    # inner loop, means the end of range index will be 9. The value 
+    # of index 9 will be 9-1, or 8.
+    for inner_loop in range(outer_loop):
+
+        # The printed result is the value of "inner_loop". Since  
+        # there aren’t any calculations in this loop, there is a 
+        # simple shortcut for solving what the final value printed 
+        # by the "inner_loop" will be. The solution is to simply use 
+        # the value of the "inner_loop" index, which is 8.
+        print(inner_loop)
+        
+
+#
+#Example-4
+# This function should count down by -2 from 11 to 1, so that it only
+# prints odd numbers. 
+
+# This range(11, -2) tells the for loop to start at 11 and end at index
+# position -2 (which corresponds to the numeric value of -1). Since the
+# third incremental or decremental value is missing, the loop will 
+# increment by the default of +1 instead of the intended -2 decrement.
+# Starting at index position 11 and incrementing by +1 will end the loop 
+# automatically, because the index is not counting down towards -2 as 
+# the end of the range. 
+
+# To fix this problem, the range() needs three parameters:
+# First parameter should be the starting index position of 11.
+# Second parameter should be the ending index position of 0 (value 1).
+# Third parameter should be decrementing by -2.
+# So, the range should be configured as range(11, 0, -2).
+
+# Fix this loop with the corrected range parameters and click Run.
+for n in range(11,1, -2):
+    if n % 2 != 0:
+        print(n, end=" ")
+
+# Should print: 11, 9, 7, 5, 3, 1 once the problem is fixed.
