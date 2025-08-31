04e7702a7245261ffe2167927e1f60a92103b927
Konvaly <konvaly@gmail.com>
2025-08-31 23:55:36 +0100

    Added file recursion with an example-4

--- recursion/recursion_examples.py
+++ recursion/recursion_examples.py
@@ -45,3 +45,27 @@ def is_power_of(number, base):
 print(is_power_of(8,2))     # Should be True
 print(is_power_of(64,4))    # Should be True
 print(is_power_of(70,10))   # Should be False
+
+# Example-4
+# Recursion is a process where a function calls itself one or more times 
+# with modified values to accomplish a task. This technique can be particularly
+#  effective when solving complex problems that can be broken down into smaller, 
+# simpler problems of the same type. In the provided code, the count_users function uses
+#  recursion to count the number of users that belong to a group within a company's system. 
+# It does this by iterating through each member of a group, and if a member is another group, 
+# it recursively calls count_users to count the users within that subgroup. However, 
+# there is a bug in the code! Can you spot the problem and fix it?
+
+def count_users(group):
+  count = 0
+  for member in get_members(group):
+    if is_group(member):
+      count += count_users(member)
+    else:
+      count += 1
+  return count
+
+
+print(count_users("sales")) # Should be 3
+print(count_users("engineering")) # Should be 8
+print(count_users("everyone")) # Should be 18
