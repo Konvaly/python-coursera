430482df54f5749cfbb2e6023ac6f48bbe20bf71
Konvaly <konvaly@gmail.com>
2025-09-04 23:39:42 +0100

    Added file quiz_loop with an example-8

--- quiz/quiz_loop.py
+++ quiz/quiz_loop.py
@@ -115,6 +115,85 @@ print(all_numbers(0,0))   # 0
 number = 15 # Initialize the variable
 while number >= 5: # Complete the while loop condition
     print(number, end=" ")
-    number -= 5 # Increment the variable
+    number -= 5 # Decrement the variable
 
 # Should print 15 10 5 
+
+
+# Task-7
+# Fill in the blanks to complete the “factorial” function. This function will accept 
+# an integer variable “n” through the function parameters and produce the factorials 
+# of this number (by multiplying this value by every number less than the original 
+# number [n*(n-1)], excluding 0).  To do this, the function should:
+#
+# - accept an integer variable “n” through the function parameters;
+#
+# - initialize a variable “result” to the value of the “n” variable;
+#
+# - iterate over the values of “n” using a while loop until “n” is equal to 0;
+#
+# - starting at n-1, multiply the result by the current “n” value;
+#
+# - decrement “n” by -1.
+#
+# -  For example, factorial 3 would return the value of 3*2*1, which would be 6.
+
+# Solution-1 with loop
+def factorial(n):
+    result = n
+    start = n
+    n -= 1
+    while n > 0: # The while loop should execute as long as n is greater than 0
+        result *= n # Multiply the current result by the current value of n
+        n -= 1 # Decrement the appropriate variable by -1
+    return result
+
+
+print(factorial(3)) # Should print 6
+print(factorial(9)) # Should print 362880
+print(factorial(1)) # Should print 1
+
+# Solution-2 with resursion
+def factorial_recursion(n):
+     # Base case: 0! and 1! are both 1
+    if n == 0 or n == 1:
+        return 1
+    # Recursive case: n! = n * (n-1)!
+    else:
+        return n * factorial_recursion(n - 1)
+
+# Examples
+print(factorial_recursion(3))  # 6
+print(factorial_recursion(5))  # 120
+print(factorial_recursion(0))  # 1
+
+
+# Task-8
+##
+# Fill in the blanks to complete the “counter” function. This function should count down
+#  from the “start” to “stop” variables when “start” is bigger than “stop”. 
+# Otherwise, it should count up from “start” to “stop”. Complete the code so that a function call
+#  like “counter(3, 1)” will return “Counting down: 3, 2, 1” and “counter(2, 5)” 
+# will return “Counting up: 2, 3, 4, 5”.
+
+def counter(start, stop):
+    if start > stop:
+        return_string = "Counting down: "
+        while start >= stop: # Complete the while loop
+            return_string += str(start) # Add the numbers to the "return_string"
+            if start > stop:
+                return_string += ","
+            start -= 1 # Increment the appropriate variable
+    else:
+        return_string = "Counting up: "
+        while stop >= start: # Complete the while loop
+            return_string += str(start) # Add the numbers to the "return_string"
+            if start < stop:
+                return_string += ","
+            start += 1 # Increment the appropriate variable
+    return return_string
+
+
+print(counter(1, 10)) # Should be "Counting up: 1,2,3,4,5,6,7,8,9,10"
+print(counter(2, 1)) # Should be "Counting down: 2,1"
+print(counter(5, 5)) # Should be "Counting up: 5"
