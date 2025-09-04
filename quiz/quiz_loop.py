7c504323dcf515014d1b8ab3265727f6841cf8fe
Konvaly <konvaly@gmail.com>
2025-09-04 18:42:00 +0100

    Added file quiz_loop with an example-1

---
+++ quiz/quiz_loop.py
@@ -0,0 +1,9 @@
+# Task-1
+# Fill in the blanks to print the even numbers from 2 to 12.
+
+number = 2 # Initialize the variable 
+while number <= 12: # Complete the while loop condition
+    print(number, end=" ")
+    number += 2 # Increment the variable
+
+# Should print 2 4 6 8 10 12 
