2431032e4663bc8354351ee74f27867899799b70
Konvaly <konvaly@gmail.com>
2025-09-04 18:49:11 +0100

    Added file quiz_loop with an example-3

--- quiz/quiz_loop.py
+++ quiz/quiz_loop.py
@@ -40,3 +40,28 @@ def factorial(n):
 print(factorial(3)) # Should print 6
 print(factorial(9)) # Should print 362880
 print(factorial(1)) # Should print 1
+
+# Task-3
+# Fill in the blanks to complete the “rows_asterisks” function. This function should print rows of asterisks (*), where the number of rows is equal to the “rows” variable. The number of asterisks per row should correspond to the row number (row 1 should have 1 asterisk, row 2 should have 2 asterisks, etc.). Complete the code so that “row_asterisks(5)” will print:
+
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+def rows_asterisks(rows):
+    # Complete the outer loop range to control the number of rows
+    for x in range(6): 
+        # Complete the inner loop range to control the number of 
+        # asterisks per row
+        for y in range(x): 
+            # Prints one asterisk and one space
+            print("*", end=" ")
+        # An empty print() function inserts a line break at the 
+        # end of the row 
+        print()
+
+
+rows_asterisks(5)
+# Should print the asterisk rows shown above
