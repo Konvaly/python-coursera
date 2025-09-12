0e404a738fab6acecbb927361968f4b82c94e696
Konvaly <konvaly@gmail.com>
2025-09-12 18:28:38 +0100

    Added example-2 in creating-string.py

--- strings/creating_string.py
+++ strings/creating_string.py
@@ -3,3 +3,12 @@
 message = "A kong string with a silly typo"
 new_message = message[0:2] + "l" + message[3:]
 print(new_message)
+
+
+# Example 2
+
+message = "This is a new message"
+print(message)
+message = "And another one"
+print(message)
+
