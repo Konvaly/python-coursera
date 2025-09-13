c7ef479755f3c3b748f14fe120b4ec3ea281f2c6
Konvaly <konvaly@gmail.com>
2025-09-13 12:30:25 +0100

    Added example in creating-string.py

--- strings/creating_string.py
+++ strings/creating_string.py
@@ -24,3 +24,17 @@ print(pets.index("s"))
 pets="Cats & Dogs"
 print("Dragons" in pets)
 print("Cats" in pets)
+
+# Example 5
+def replace_domain(email, old_domain, new_domain):
+  if "@" + old_domain in email:
+    index = email.index("@" + old_domain)
+    new_email = email[:index] + "@" + new_domain
+    return print(new_email)
+  return print(email)
+
+replace_domain("asd@fig.com", "fig.com", "gmail.com")  # Returns "
+
+
+#Example
+
